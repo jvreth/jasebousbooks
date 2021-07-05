68d277c000acdbf30e2275d6c4d0e62a918fe531
Jasmin von Reth <jasmin.vonreth@rewe-group.com>
2021-07-05 16:11:52 +0200

    removed instagram plugin

--- src/components/organisms/instagram-feed/instagram.js
+++ src/components/organisms/instagram-feed/instagram.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Instagram from '../../atoms/instagram'
-import Container from './instagram.css'
+// import React from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import Instagram from '../../atoms/instagram'
+// import Container from './instagram.css'
 
-const CLASS = 'insta'
+// const CLASS = 'insta'
 
-const InstagramFeed = () => (
-  <>
-    <Container>
-    <div className="row row--center">
-      <a
-        className={`${CLASS}__link`}
-        href="https://instagram.com/jasebousbooks"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <span className={`${CLASS}__link-icon`}>
-          <FontAwesomeIcon icon={['fab', 'instagram']} />
-        </span>
-        <span className={`${CLASS}__link-text`}>Me on Instagram</span>
-      </a>
-    </div>
-    <div className="row">
-      <Instagram className={`${CLASS}__post`} />
-    </div>
-    </Container>
-  </>
-)
+// const InstagramFeed = () => (
+//   <>
+//     <Container>
+//     <div className="row row--center">
+//       <a
+//         className={`${CLASS}__link`}
+//         href="https://instagram.com/jasebousbooks"
+//         target="_blank"
+//         rel="noreferrer"
+//       >
+//         <span className={`${CLASS}__link-icon`}>
+//           <FontAwesomeIcon icon={['fab', 'instagram']} />
+//         </span>
+//         <span className={`${CLASS}__link-text`}>Me on Instagram</span>
+//       </a>
+//     </div>
+//     <div className="row">
+//       <Instagram className={`${CLASS}__post`} />
+//     </div>
+//     </Container>
+//   </>
+// )
 
-export default InstagramFeed
+// export default InstagramFeed
