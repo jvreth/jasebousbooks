afcdb23ecb6b067adc0caa315f0704c0c8689644
Jasmin von Reth <jasmin.vonreth@rewe-group.com>
2022-05-02 15:49:49 +0200

    Fix typos in legend page

--- src/pages/legend.js
+++ src/pages/legend.js
@@ -38,7 +38,7 @@ const Legend = () => (
           Fünf Sterne bekommt ein Buch von mir nur dann, wenn es für mich ein Highlight war. 
           Ein Highlight ist für mich ein Buch, dass mich ständig beschäftigt, egal ob ich es gerade lese oder nicht. 
           Wenn ich ständig über das Buch nachdenken muss, mir ausmale was als nächstes passiert und ich in Gedanken eigentlich immer zu einem Teil bei der Geschichte und den Charakteren bin. 
-          Die Bücher haben mich in der Regel auch unglaublich viel fühlen lassen und es sind Bücher die ich nicht so leicht vergessen werde.
+          Die Bücher haben mich in der Regel auch unglaublich viel fühlen lassen und es sind Bücher, die ich nicht so leicht vergessen werde.
         </p>
         <hr/>
       </StyledSection>
@@ -49,8 +49,8 @@ const Legend = () => (
         <FontAwesomeIcon className={`${CLASS}__rating-icon`} icon={['fas', 'star']} />
         <FontAwesomeIcon className={`${CLASS}__rating-icon`} icon={['fas', 'star']} />
         <p>
-          Vier Sterne gebe ich Büchern die mir gut bis sehr gut gefallen haben. 
-          Die mich gut unterhalten haben, wo ich die Charaktere mochte, die Handlung mich auch mitgenommen hat, es jedoch einige Sachen gab die mich gestört haben oder mir sogar gefehlt haben um das Highlight-Gefühl zu wecken. 
+          Vier Sterne gebe ich Büchern, die mir gut bis sehr gut gefallen haben. 
+          Die mich gut unterhalten haben, wo ich die Charaktere mochte, die Handlung mich auch mitgenommen hat, es jedoch einige Sachen gab, die mich gestört haben oder mir sogar gefehlt haben, um das Highlight-Gefühl zu wecken. 
           Ein Buch mit vier Sternen ist für mich ein solides und gutes Buch und ich empfehle es in der Regel auch gerne weiter.
         </p>
         <hr/>
@@ -62,8 +62,8 @@ const Legend = () => (
         <FontAwesomeIcon className={`${CLASS}__rating-icon`} icon={['fas', 'star']} />
         <p>
           Bücher mit einer drei Sterne Bewertung find ich leider nur "okay". 
-          Oft haben mich die Bücher zwar unterhalten können, aber es gab viele Stellen die mich gestört haben. 
-          Ob es nun die Charaktere waren zu denen ich keine Bindung aufbauen konnte oder die Handlung für mich teilweise nicht nachvollziehbar war. 
+          Oft haben mich die Bücher zwar unterhalten können, aber es gab viele Stellen, die mich gestört haben. 
+          Ob es nun die Charaktere waren, zu denen ich keine Bindung aufbauen konnte oder die Handlung für mich teilweise nicht nachvollziehbar war. 
           Drei Sterne Bücher kann ich oft nur eingeschränkt weiterempfehlen.
         </p>
         <hr/>
@@ -74,8 +74,9 @@ const Legend = () => (
         <FontAwesomeIcon className={`${CLASS}__rating-icon`} icon={['fas', 'star']} />
         <p>
           Zwei Sterne Bücher gibt es bei mir nur selten, trotzdem passiert es ab und zu das ich einem Buch leider diese Bewertung geben muss. 
-          Bücher mit dieser Bewertung fand ich von vorne bis hinten meist schlecht. Es gab zu viel das mich gestört hat und mir deshalb den spaß am Lesen genommen hat. 
-          Es ist oft eine Mischung aus fehlenden Verständnis gegenüber den Charakteren und einer Handlung die ich zum Großteil langweilig fand.
+          Bücher mit dieser Bewertung fand ich von vorne bis hinten meist schlecht. 
+          Es gab zu viel das mich gestört hat und mir deshalb den Spaß am Lesen genommen hat. 
+          Es ist oft eine Mischung aus fehlenden Verständnis gegenüber den Charakteren und einer Handlung, die ich zum Großteil langweilig fand. 
           Diese Bücher kann ich in der Regel nur sehr eingeschränkt weiterempfehlen, zum Beispiel wenn ich merke das ich nicht die Zielgruppe war.
         </p>
         <hr/>
@@ -84,10 +85,10 @@ const Legend = () => (
         <h2>1 Stern</h2>
         <FontAwesomeIcon className={`${CLASS}__rating-icon`} icon={['fas', 'star']} />
         <p>
-          Es passiert noch seltener, das ein Buch von mir nur einen Stern bekommt. 
+          Es passiert noch seltener, dass ein Buch von mir nur einen Stern bekommt. 
           Diese Bücher habe ich nämlich abgebrochen und konnte die Geschichte nicht weiterlesen. 
           Ich breche wirklich sehr selten Bücher ab und wenn ich es tue, dann hat mir das Buch wirklich überhaupt nicht gefallen. 
-          Diese Bücher kann ich dann auch wirklich niemanden weiterempfehlen, da ich sie auch nicht beendet habe.
+          Diese Bücher kann ich dann auch niemanden weiterempfehlen, da ich sie auch nicht beendet habe.
         </p>
       </StyledSection>
     </div>
